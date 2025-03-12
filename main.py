abbd33358e1342d18fbf54b51850d3cf497fa451
kawthar-mohammed <Kawtharmoh039@gmail.com>
2025-03-12 18:39:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
-from typing import Optional
-
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+import joblib
+model = joblib.load('knn_model.joblib')
+scaler = joblib.load('scaler.joblib')
 
 app = FastAPI()
-
-
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def read_root():
+    return {"message": "Welcome to Tuwaiq Academy"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+class InputFeatures(BaseModel):
+    minutes_played:int
+    highest_value: int
+
+def preprocessing(input_features: InputFeatures):
+    dict_f = {
+        'minutes_played': input_features.minutes_played,
+        'highest_value': input_features.highest_value,
+        
+    }
+    features_list = [dict_f[key] for key in sorted(dict_f)]
+    scaled_features = scaler.transform([list(dict_f.values())])
+    return scaled_features.tolist()
+
+@app.post("/predict")
+async def predict(input_features: InputFeatures):
+    data = preprocessing(input_features)
+    y_pred = model.predict(data)
+    return {"pred": y_pred.tolist()[0]} 
